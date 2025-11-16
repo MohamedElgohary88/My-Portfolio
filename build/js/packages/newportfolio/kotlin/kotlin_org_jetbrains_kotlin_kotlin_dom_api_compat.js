e1f2ec876c97959c28a92c6f049165c3bd578b6e
mohamedelgohary <mohamed.abdelazim.elgohary@gmail.com>
2025-11-17 01:18:41 +0200

    chore: include updated build artifacts and static export for deployment

--- build/js/packages/newportfolio/kotlin/kotlin_org_jetbrains_kotlin_kotlin_dom_api_compat.js
+++ build/js/packages/newportfolio/kotlin/kotlin_org_jetbrains_kotlin_kotlin_dom_api_compat.js
@@ -12,22 +12,21 @@
 }(this, function (_, kotlin_kotlin) {
   'use strict';
   //region block: imports
-  var Regex_init_$Create$ = kotlin_kotlin.$_$.d1;
-  var toSet = kotlin_kotlin.$_$.e9;
-  var THROW_CCE = kotlin_kotlin.$_$.ff;
-  var isCharSequence = kotlin_kotlin.$_$.bb;
-  var trim = kotlin_kotlin.$_$.ne;
-  var toString = kotlin_kotlin.$_$.xb;
-  var ArrayList_init_$Create$ = kotlin_kotlin.$_$.l;
-  var joinToString = kotlin_kotlin.$_$.g7;
-  var StringBuilder_init_$Create$ = kotlin_kotlin.$_$.e1;
-  var charSequenceLength = kotlin_kotlin.$_$.ja;
-  var joinTo = kotlin_kotlin.$_$.i7;
-  var ensureNotNull = kotlin_kotlin.$_$.yf;
-  var protoOf = kotlin_kotlin.$_$.ub;
-  var classMeta = kotlin_kotlin.$_$.ka;
-  var setMetadataFor = kotlin_kotlin.$_$.vb;
-  var VOID = kotlin_kotlin.$_$.f;
+  var Regex_init_$Create$ = kotlin_kotlin.$_$.x;
+  var toSet = kotlin_kotlin.$_$.c5;
+  var THROW_CCE = kotlin_kotlin.$_$.p9;
+  var isCharSequence = kotlin_kotlin.$_$.q6;
+  var trim = kotlin_kotlin.$_$.d9;
+  var toString = kotlin_kotlin.$_$.c7;
+  var ArrayList_init_$Create$ = kotlin_kotlin.$_$.j;
+  var Unit_instance = kotlin_kotlin.$_$.h2;
+  var joinToString = kotlin_kotlin.$_$.u3;
+  var StringBuilder_init_$Create$ = kotlin_kotlin.$_$.y;
+  var charSequenceLength = kotlin_kotlin.$_$.b6;
+  var joinTo = kotlin_kotlin.$_$.w3;
+  var protoOf = kotlin_kotlin.$_$.z6;
+  var classMeta = kotlin_kotlin.$_$.c6;
+  var setMetadataFor = kotlin_kotlin.$_$.a7;
   //endregion
   //region block: pre-declaration
   setMetadataFor(EventListenerHandler, 'EventListenerHandler', classMeta);
@@ -37,7 +36,7 @@
     var this_0 = _this__u8e3s4.className;
     // Inline function 'kotlin.text.toRegex' call
     var this_1 = '(^|.*\\s+)' + cssClass + '($|\\s+.*)';
-    return Regex_init_$Create$(this_1).matches_evli6i_k$(this_0);
+    return Regex_init_$Create$(this_1).p8(this_0);
   }
   function removeClass(_this__u8e3s4, cssClasses) {
     var tmp$ret$1;
@@ -65,14 +64,14 @@
       var this_1 = toString(trim(isCharSequence(this_0) ? this_0 : THROW_CCE()));
       // Inline function 'kotlin.text.toRegex' call
       // Inline function 'kotlin.collections.filterTo' call
-      var this_2 = Regex_init_$Create$('\\s+').split_p7ck23_k$(this_1, 0);
+      var this_2 = Regex_init_$Create$('\\s+').t8(this_1, 0);
       var destination = ArrayList_init_$Create$();
-      var tmp0_iterator = this_2.iterator_jk1svi_k$();
-      while (tmp0_iterator.hasNext_bitz1p_k$()) {
-        var element_0 = tmp0_iterator.next_20eer_k$();
+      var tmp0_iterator = this_2.l();
+      while (tmp0_iterator.z()) {
+        var element_0 = tmp0_iterator.b1();
         // Inline function 'kotlinx.dom.removeClass.<anonymous>' call
-        if (!toBeRemoved.contains_aljjnj_k$(element_0)) {
-          destination.add_utx5q5_k$(element_0);
+        if (!toBeRemoved.o(element_0)) {
+          destination.w(element_0);
         }
       }
       _this__u8e3s4.className = joinToString(destination, ' ');
@@ -91,12 +90,12 @@
       inductionVariable = inductionVariable + 1 | 0;
       // Inline function 'kotlinx.dom.addClass.<anonymous>' call
       if (!hasClass(_this__u8e3s4, element)) {
-        destination.add_utx5q5_k$(element);
+        destination.w(element);
       }
     }
     var missingClasses = destination;
     // Inline function 'kotlin.collections.isNotEmpty' call
-    if (!missingClasses.isEmpty_y1axqb_k$()) {
+    if (!missingClasses.q()) {
       // Inline function 'kotlin.text.trim' call
       var this_0 = _this__u8e3s4.className;
       var presentClasses = toString(trim(isCharSequence(this_0) ? this_0 : THROW_CCE()));
@@ -106,10 +105,10 @@
       var this_1 = StringBuilder_init_$Create$();
       // Inline function 'kotlin.contracts.contract' call
       // Inline function 'kotlinx.dom.addClass.<anonymous>' call
-      this_1.append_22ad7x_k$(presentClasses);
+      this_1.v5(presentClasses);
       // Inline function 'kotlin.text.isEmpty' call
       if (!(charSequenceLength(presentClasses) === 0)) {
-        this_1.append_22ad7x_k$(' ');
+        this_1.v5(' ');
       }
       joinTo(missingClasses, this_1, ' ');
       _this__u8e3s4.className = this_1.toString();
@@ -117,100 +116,27 @@
     }
     return false;
   }
-  function clear(_this__u8e3s4) {
-    while (_this__u8e3s4.hasChildNodes()) {
-      _this__u8e3s4.removeChild(ensureNotNull(_this__u8e3s4.firstChild));
-    }
-  }
   function EventListener(handler) {
     return new EventListenerHandler(handler);
   }
-  function _get_handler__z70553($this) {
-    return $this.handler_1;
-  }
   function EventListenerHandler(handler) {
-    this.handler_1 = handler;
+    this.eh_1 = handler;
   }
-  protoOf(EventListenerHandler).handleEvent_zcjn50_k$ = function (event) {
-    this.handler_1(event);
+  protoOf(EventListenerHandler).fh = function (event) {
+    this.eh_1(event);
   };
   protoOf(EventListenerHandler).handleEvent = function (event) {
-    return this.handleEvent_zcjn50_k$(event);
+    return this.fh(event);
   };
   protoOf(EventListenerHandler).toString = function () {
-    return 'EventListenerHandler(' + this.handler_1 + ')';
+    return 'EventListenerHandler(' + this.eh_1 + ')';
   };
-  function get(_this__u8e3s4, index) {
-    // Inline function 'kotlin.js.asDynamic' call
-    return _this__u8e3s4[index];
-  }
-  function EventSourceInit(withCredentials) {
-    withCredentials = withCredentials === VOID ? false : withCredentials;
-    var o = {};
-    o['withCredentials'] = withCredentials;
-    return o;
-  }
-  function get_0(_this__u8e3s4, index) {
-    // Inline function 'kotlin.js.asDynamic' call
-    return _this__u8e3s4[index];
-  }
-  function get_1(_this__u8e3s4, index) {
-    // Inline function 'kotlin.js.asDynamic' call
-    return _this__u8e3s4[index];
-  }
-  function MutationObserverInit(childList, attributes, characterData, subtree, attributeOldValue, characterDataOldValue, attributeFilter) {
-    childList = childList === VOID ? false : childList;
-    attributes = attributes === VOID ? undefined : attributes;
-    characterData = characterData === VOID ? undefined : characterData;
-    subtree = subtree === VOID ? false : subtree;
-    attributeOldValue = attributeOldValue === VOID ? undefined : attributeOldValue;
-    characterDataOldValue = characterDataOldValue === VOID ? undefined : characterDataOldValue;
-    attributeFilter = attributeFilter === VOID ? undefined : attributeFilter;
-    var o = {};
-    o['childList'] = childList;
-    o['attributes'] = attributes;
-    o['characterData'] = characterData;
-    o['subtree'] = subtree;
-    o['attributeOldValue'] = attributeOldValue;
-    o['characterDataOldValue'] = characterDataOldValue;
-    o['attributeFilter'] = attributeFilter;
-    return o;
-  }
-  function RequestInit(method, headers, body, referrer, referrerPolicy, mode, credentials, cache, redirect, integrity, keepalive, window_0) {
-    method = method === VOID ? undefined : method;
-    headers = headers === VOID ? undefined : headers;
-    body = body === VOID ? undefined : body;
-    referrer = referrer === VOID ? undefined : referrer;
-    referrerPolicy = referrerPolicy === VOID ? undefined : referrerPolicy;
-    mode = mode === VOID ? undefined : mode;
-    credentials = credentials === VOID ? undefined : credentials;
-    cache = cache === VOID ? undefined : cache;
-    redirect = redirect === VOID ? undefined : redirect;
-    integrity = integrity === VOID ? undefined : integrity;
-    keepalive = keepalive === VOID ? undefined : keepalive;
-    window_0 = window_0 === VOID ? undefined : window_0;
-    var o = {};
-    o['method'] = method;
-    o['headers'] = headers;
-    o['body'] = body;
-    o['referrer'] = referrer;
-    o['referrerPolicy'] = referrerPolicy;
-    o['mode'] = mode;
-    o['credentials'] = credentials;
-    o['cache'] = cache;
-    o['redirect'] = redirect;
-    o['integrity'] = integrity;
-    o['keepalive'] = keepalive;
-    o['window'] = window_0;
-    return o;
-  }
   //region block: exports
   _.$_$ = _.$_$ || {};
   _.$_$.a = addClass;
-  _.$_$.b = clear;
-  _.$_$.c = hasClass;
-  _.$_$.d = removeClass;
-  _.$_$.e = EventListener;
+  _.$_$.b = hasClass;
+  _.$_$.c = removeClass;
+  _.$_$.d = EventListener;
   //endregion
   return _;
 }));
